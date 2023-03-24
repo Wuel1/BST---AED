6ad6c86bbd09a0da351527455b31bbf7f5f66976
Wandson Emanuel <wandson7emanuel@hotmail.com>
2023-03-24 20:47:04 -0300

    Delete Completo e Modificado

--- BST.java
+++ BST.java
@@ -46,46 +46,37 @@ public class BST implements EstruturaDeDados{
     }
 
     private Node deleteNode(Node n, int key){
-        if (key >= n.getValue()){
-            Node r = n.getRight();
-            if (r.getValue() == key){
-                //verificar se r é folha
-                if (r.getRight() == null && r.getLeft() == null){
-                    //Caso 1
-                    n.setRight(null);
-                   
-                    
-                } else if (r.getRight() == null){
-                    //Caso 2
-                    n.setRight(r.getLeft());
-                    
-                    
-                } else if (r.getLeft() == null){
-                    //Caso 2
-                    n.setRight(r.getRight());
-                    
-                    
-                } else{
-                    Node troca = minNo(r);
-                    n.setValue(troca.getValue());
-                    n.setRight(deleteNode(r, troca.getValue()));
-                }        
+        if (n == null) {
+            return null;
+        }    
+        if (key < n.getValue()) {
+            n.setLeft(deleteNode(n.getLeft(), key));
+        } else if (key > n.getValue()) {
+            n.setRight(deleteNode(n.getRight(), key));
+        } else {
+            if (n.getLeft() == null) {
+                return n.getRight();
+            } else if (n.getRight() == null) {
+                return n.getLeft();
             }
+    
+            Node temporario = minNo(n.getRight());
+            n.setValue(temporario.getValue());
+            n.setRight(deleteNode(n.getRight(), temporario.getValue()));
         }
+    
         return n;
-        
-        
     }
-
-
+    
     public Node minNo(Node n){
-        n.atual = n;
-        while(n.atual.getLeft() != null){
-            n.atual = n.atual.getLeft();
+        Node nAtual = n;
+        while(nAtual.getLeft() != null){
+            nAtual = nAtual.getLeft();
         }
-        return n.atual;
-    }
+        return nAtual;
+    }   
 
+    
     @Override
     public boolean search(int key) {
         if (root == null){
@@ -112,6 +103,26 @@ public class BST implements EstruturaDeDados{
         }
     }
 
+    // public Node buscarPai(Node raiz, int valorBuscado) {
+    //     if (raiz == null || raiz.getValue() == valorBuscado) {
+    //         return null; // árvore vazia ou valor encontrado na raiz
+    //     }
+        
+    //     if ((raiz.getLeft() != null && raiz.getLeft().getValue() == valorBuscado) || 
+    //         (raiz.getRight() != null && raiz.getRight().getValue() == valorBuscado)) {
+    //         return raiz; // encontramos o nó filho com o valor buscado, retornamos o pai
+    //     }
+        
+    //     Node pai = buscarPai(raiz.getLeft(), valorBuscado); // busca recursiva no filho esquerdo
+        
+    //     if (pai == null) {
+    //         pai = buscarPai(raiz.getRight(), valorBuscado); // busca recursiva no filho direito
+    //     }
+        
+    //     return pai;
+    // }
+     
+
     @Override
     public int minimum() {
         return 0;
@@ -134,19 +145,20 @@ public class BST implements EstruturaDeDados{
 
     public static void main(String[] args) {
         BST tree = new BST();
-        System.out.println(tree.search(7));
-        tree.insert(4);
-        tree.insert(2);
+        System.out.println(tree.search(7)); // false
+        tree.insert(8);
         tree.insert(5);
+        tree.insert(11);
+        tree.insert(10);
         tree.insert(1);
         tree.insert(3);
         tree.insert(6);
-        System.out.println(tree.search(5));
-        tree.delete(5);
-        System.out.println(tree.search(5));
-        System.out.println(tree.search(6));
+        System.out.println(tree.search(10)); //true
+        tree.delete(10);
+        System.out.println(tree.search(10)); // false
+        System.out.println(tree.search(6)); // true
         tree.delete(6);
-        System.out.println(tree.search(6));
+        System.out.println(tree.search(6)); // false
         
     }
 }
